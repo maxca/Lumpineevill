4dc7c04bc222f08b3721f54700c474766f59d3b6
Samark Chaisanguan <samarkchsngn@gmail.com>
2018-05-30 23:28:15 +0700

    fixed name space

--- src/Generate/TemplateAPI/Controller.php
+++ src/Generate/TemplateAPI/Controller.php
@@ -6,43 +6,66 @@
 namespace App\Http\Controllers\{replace};
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\{replace}\{replace}Request;
-use App\Http\Requests\{replace}\{replace}GetRequest;
-use App\Http\Requests\{replace}\{replace}CreateRequest;
-use App\Http\Requests\{replace}\{replace}UpdateRequest;
-use App\Http\Requests\{replace}\{replace}DeleteRequest;
+use App\Http\Requests\{replace}\Get{replace}Request;
+use App\Http\Requests\{replace}\Create{replace}Request;
+use App\Http\Requests\{replace}\Update{replace}Request;
+use App\Http\Requests\{replace}\Delete{replace}Request;
 use App\Repository\{replace}\{replace}Repository;
 
 class {replace}Controller extends Controller
 {
 
     /**
-     * set {replace_sm}
-     * @var collection
+     * set {replace_sm} repository
+     * @var object
      */
     protected ${replace_sm};
+
 
     public function __construct({replace}Repository ${replace_sm})
     {
         $this->{replace_sm} = ${replace_sm};
     }
 
-    public function create{replace}({replace}CreateRequest $request)
+    /**
+     * [create{replace} description]
+     * @param  {replace}CreateRequest $request [description]
+     * @return [type]                          [description]
+     */
+    public function create{replace}(Create{replace}Request $request)
     {
         $query = $this->{replace_sm}->createData($request->all());
         return response()->json($query); 
     }
-    public function get{replace}List({replace}GetRequest $request)
+
+    /**
+     * [get{replace} description]
+     * @param  {replace}GetRequest $request [description]
+     * @return [type]                          [description]
+     */
+    public function get{replace}List(Get{replace}Request $request)
     {
         $query = $this->{replace_sm}->search($request->all())->getData();
         return response()->json($query);   
     }
-    public function delete{replace}({replace}DeleteRequest $request)
+
+    /**
+     * [delete{replace} description]
+     * @param  {replace}DeleteRequest $request [description]
+     * @return [type]                          [description]
+     */
+    public function delete{replace}(Delete{replace}Request $request)
     {   
         $query = $this->{replace_sm}->delete($request->all());
         return response()->json($query);
     }
-    public function update{replace}({replace}UpdateRequest $request)
+
+    /**
+     * [update{replace} description]
+     * @param  {replace}UpdateRequest $request [description]
+     * @return [type]                          [description]
+     */
+    public function update{replace}(Update{replace}Request $request)
     {
         $query = $this->{replace_sm}->updateData($request->all());
         return response()->json($query);   
