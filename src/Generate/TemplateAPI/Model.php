c66aec7c2c43666507826c9fb6b98ae5eae2976a
Samark Chaisanguan <samarkchsngn@gmail.com>
2018-05-30 23:32:08 +0700

    fixed model

--- src/Generate/TemplateAPI/Model.php
+++ src/Generate/TemplateAPI/Model.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class {replace}Model extends Model
 {
-	# use SoftDeletes;
+    # use SoftDeletes;
 
     protected $fillable = [
         'id',
@@ -21,9 +21,29 @@ class {replace}Model extends Model
         'created_at',
         'updated_at',
     ];
+
+    /**
+     * set table name
+     * @var string
+     */
     protected $table = '{replace_snc}';
 
-    
+    /**
+     * set casting value
+     * @var array
+     */
+    protected $casts = [];
 
+    /**
+     * set append attribute
+     * use get{NameColumn}Attribute function
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * set datetime column
+     * @var array
+     */
     protected $dates = ['created_at', 'updated_at'];
 }
