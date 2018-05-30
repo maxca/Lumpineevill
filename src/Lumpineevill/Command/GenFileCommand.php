63d434ebdfda966939eaff13e99698b91c864114
Samark Chaisanguan <samarkchsngn@gmail.com>
2018-05-30 18:38:19 +0700

    add genFIle

---
+++ src/Lumpineevill/Command/GenFileCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Lumpineevill\Command;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Lumpineevill\Generate\GenerateAPI;
+
+class GenFileCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'samark:genfile';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate file';
+
+    /**
+     * winner list
+     * @var collection
+     */
+    protected $generate;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        Log::info('start process ' . get_class());
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $namespace = $this->ask('What is your namespace ?');
+        $this->generate = app(GenerateAPI::class, ['namespace' => $namespace]);
+        $this->generate->setPath(base_path());
+        $this->generate->excute();
+
+    }
+}
